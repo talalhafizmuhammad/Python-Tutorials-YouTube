57da41897387572829211772ce6b2d27f4ecf3d4
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2026-08-10 17:48:50 +0500

    Add variable examples and print statements
    Added variable declarations and print statements demonstrating different ways to display age and values.

---
+++ Variebles.py
@@ -0,0 +1,23 @@
+age = 20
+my1_age = 93
+#1age = 20 wrong format
+age = 2.74
+Age = 19
+# simple method
+#fstring method
+
+print("age is: ", age)
+print("age is: ", Age)
+print("age is: ", my1_age)
+
+print(f"age is: {age}")
+
+If = 24
+
+a, b, c = 10, 20, 30
+print(f"Vals: {a} {b} {c}")
+
+x = y = z = 100
+print(f"Vals: {x} {y} {z}")
+
+# Class 
