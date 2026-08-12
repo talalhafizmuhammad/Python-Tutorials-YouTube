b2088a528bf75a2e5c7a1053643d197eeac7c46d
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2026-08-12 18:38:19 +0500

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,26 @@
+a = 5
+
+
+# print(a // b) calculates floor division
+# print(a % b) remainder
+# print(a ** b) exponential
+# c = a + b
+# print(c)
+
+# a -> 7
+
+# a += 2 same as a = a + 2
+# print(a)
+b = 2
+a %= b
+print(a)
+
+a, b = 10, 10
+
+print(a <= b)
+
+a, b, c, d = 2, 4, 4, 4
+print(a > b or c > d)
+
+print(not(a < b))
+print('5' == 5)
