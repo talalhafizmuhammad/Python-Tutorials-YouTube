da5fc1f3b1cf66a4ca616322f9f2cda87d216f06
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2026-08-19 12:59:08 +0500

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,28 +1,28 @@
-# i = 1
-# while i <= 15:
-#     print("Running")
-#     i += 1
-#     if i == 10:
-#         continue
-#     print("Hy")
-# print("Done")
+i = 1
+while i <= 15:
+    print("Running")
+    i += 1
+    if i == 10:
+        continue
+    print("Hy")
+print("Done")
 
-# for i in range(5):
-#     print(i, end=" ")
+for i in range(5):
+    print(i, end=" ")
 
-# for i in range(0, 10, 3):
-#     print(i)
+for i in range(0, 10, 3):
+    print(i)
 
-# string = "Python"
-# for i in string:
-#     print(i)
+string = "Python"
+for i in string:
+    print(i)
 
-# for row in range(3):
-#     for col in range(3):
-#         print("*", end="")
-#     print() # for next line
+for row in range(3):
+    for col in range(3):
+        print("*", end="")
+    print() # for next line
 
 n = int(input("Enter a number: "))
 for i in range(1, 11):
-    # print(f"{n} x {i} = {n*i}")
+    print(f"{n} x {i} = {n*i}")
     print(n*i)
