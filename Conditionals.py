12ce47d4e6e26ca30cdf0b727794ebbac8c76c06
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2026-08-19 12:59:47 +0500

    Restore and update conditional statements in Conditionals.py

--- Conditionals.py
+++ Conditionals.py
@@ -1,27 +1,27 @@
-# perage = float(input("Enter ur percentage: "))
+perage = float(input("Enter ur percentage: "))
 
-# if perage >= 90:
-#     print("A+")
-# elif perage >= 80:
-#     print("A")
-# elif perage >= 70:
-#     print("B")
-# elif perage >= 60:
-#     print("C")
-# elif perage >= 50:
-#     print("D")
-# else:
-#     print("F")
+if perage >= 90:
+    print("A+")
+elif perage >= 80:
+    print("A")
+elif perage >= 70:
+    print("B")
+elif perage >= 60:
+    print("C")
+elif perage >= 50:
+    print("D")
+else:
+    print("F")
 
 grade = 92
 
-# if grade >= 90:
-#     if grade >= 95:
-#         print("Exceptional")
-#     else:
-#         print("Excellent")
-# if "None":
-#     print("Hy")
+if grade >= 90:
+    if grade >= 95:
+        print("Exceptional")
+    else:
+        print("Excellent")
+if "None":
+    print("Hy")
 name = ""
 if name:
     print("Hello,", name)
